85e2fdfd3711f4b1fe241df1dd792f3c0002d992
neeraj <neerajy@dewsolutions.in>
2017-08-28 15:54:46 +0530

    adding routes thru react-loadable

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,26 @@
 // src/routes.js
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import asyncComponent from './components/AsyncComponent';
+//import asyncComponent from './components/AsyncComponent';
+import Loadable from 'react-loadable';
+import MyLoadingComponent from './components/MyLoadingComponent';
 
-const AsyncIndex = asyncComponent(() => import('./components/App'));
-const AsyncAbout = asyncComponent(() => import('./components/About'));
-const AsyncNotFound = asyncComponent(() => import('./components/NotFound'));
+var AsyncIndex = Loadable({
+  loader: () => import('./components/App'),
+  loading: MyLoadingComponent
+});
+const AsyncAbout = Loadable({
+  loader: () => import('./components/About'),
+  loading: MyLoadingComponent
+});
+const AsyncNotFound = Loadable({
+  loader: () => import('./components/NotFound'),
+  loading: MyLoadingComponent
+});
+
+// const AsyncIndex = asyncComponent(() => import('./components/App'));
+// const AsyncAbout = asyncComponent(() => import('./components/About'));
+// const AsyncNotFound = asyncComponent(() => import('./components/NotFound'));
 
 const Routes = (props) => (
   <BrowserRouter>
